79c7834cacbd51020db064882675b2fe908e047f
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-03-10 01:44:23 +0600

    fix seeding

--- next.js/prisma/seed.js
+++ next.js/prisma/seed.js
@@ -1,8 +1,23 @@
 import { PrismaClient } from "@prisma/client";
-
 import { faker } from "@faker-js/faker";
 
-export const billboards = (count = 10) => {
+const log = message => {
+    const max_len = 10;
+
+    const split = message.split("] ");
+    const tag = split[0];
+    const space_len = max_len - tag.length;
+
+    let spaces = "";
+    for (let i = 0; i < space_len; i++) {
+        spaces += " ";
+    }
+
+    console.log(`${tag}]${spaces}${split[1]}`);
+};
+
+const billboards = (count = 10) => {
+    log(`[generate] Generating ${count} billboards...`);
     const billboards = [];
     for (let i = 0; i < count; i++) {
         const billboard = {
@@ -15,26 +30,25 @@ export const billboards = (count = 10) => {
         };
         billboards.push(billboard);
     }
+    log(`[generate] Generated ${billboards.length} billboards`);
     return billboards;
 };
 
 const prisma = new PrismaClient();
+log(`[database] Connected to the database`);
 
-const load = async () => {
-    try {
-        await prisma.billboard.deleteMany();
-        console.log("Deleted records in billboards table");
+try {
+    log(`[delete] Deleting existing records in billboards table...`);
+    await prisma.billboard.deleteMany();
+    log(`[delete] Deleted records in billboards table`);
 
-        await prisma.billboard.createMany({
-            data: billboards(100),
-        });
-        console.log("Added billboards data");
-    } catch (e) {
-        console.error(e);
-        process.exit(1);
-    } finally {
-        await prisma.$disconnect();
-    }
-};
-
-load();
+    log(`[create] Adding new billboards data...`);
+    await prisma.billboard.createMany({ data: billboards(100) });
+    log(`[create] Added billboards data`);
+} catch (error) {
+    log(`[Error]`, error);
+    process.exit(1);
+} finally {
+    await prisma.$disconnect();
+    log(`[database] Disconnected from the database`);
+}
