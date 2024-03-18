fa7df902a8cde30241ea4e9b05bd5d48e643f47a
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-03-18 16:05:25 +0600

    admin layout on laravel app

---
+++ laravel/resources/js/pages/admin/Overview.tsx
@@ -0,0 +1,9 @@
+import AdminLayout from "@/layouts/AdminLayout";
+
+const Overview = () => {
+    return <div>Overview</div>;
+};
+
+Overview.layout = (page: JSX.Element) => <AdminLayout children={page} />;
+
+export default Overview;
